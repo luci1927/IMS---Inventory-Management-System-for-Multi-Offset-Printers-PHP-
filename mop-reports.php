e53c85b177ae3a9bb5586a9990566d79907fbdbc
TharinDu DulanKa <tharindudulanka2020@gmail.com>
2024-10-30 09:00:51 +0530

    title changed

--- mop-reports.php
+++ mop-reports.php
@@ -8,7 +8,7 @@ include 'mop_session_check.php';
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Daily Reports</title>
+    <title>Inventory Management</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
     <link rel="stylesheet" href="assets/css/styles.css" />
     <link rel="icon" href="assets/favicon/favicon.png" type="image/png">
